1b160fee2d075a05e9f6eae3cc42dfa495245c78
mayds1010 <mayds1010@gmail.com>
2022-11-17 12:42:38 +0900

    commit

--- docs/vanilla_js/functions.js
+++ docs/vanilla_js/functions.js
@@ -4,6 +4,7 @@
 //} 변수의 내용이 똑같아도 밖에서는 사용할 수 없음
 //function은 코드를 재사용하기 위해 사용
 
+// normal Function
 //()안에 있는것 param(매개변수) : 2개(1, 3), return 1개(1 + 3)
 function sum(item_first, item_secound) {
   //param(매개변수) 2개(1, 3)를 이름을 만들어 안에 넣어 줌
@@ -19,3 +20,39 @@ function sum(item_first, item_secound) {
   // sum_function:리턴받는 부분
   console.log();
 }
+// anonymouse Function(익명Function)
+// let calculateSum = function (item_first, item_secound) {
+//   return item_first + item_secound;
+// };
+// {
+//   console.log(`${calculateSum(5, 7)}`);
+// }
+// Arrow Function(화살 Function)가장 많이 씀
+// let calculateSumSecound = (item_first, item_secound) => {
+//   return item_first + item_secound;
+// };
+// {
+//   console.log(`${calculateSumSecound(7, 8)}`);
+// }
+//result를 써서 출력할 수 있지만 바로 하면 코드의 단순화
+
+//rest parameters
+function printRestParams(...args) {
+  let restparam = (arg) => {
+    console.log(`arg :${arg}`);
+  };
+  args.forEach(restparam);
+}
+{
+  printRestParams(2, 4, 5);
+  printRestParams(2, 4, 5, 6, 7);
+}
+// arg :2
+// arg :4
+// arg :5
+// arg :2
+// arg :4
+// arg :5
+// arg :6
+// arg :7
+//1,2,4,3,5
